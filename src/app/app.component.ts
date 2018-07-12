c594ca1e7c8bfa7a67e1b95a882a8dc4b36f416b
uriegel <uriegel@hotmail.de>
2018-07-12 13:21:35 +0200

    IScroll in test app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core'
 import { SetupService } from 'umobilekit'
 
+declare class IScroll {
+    constructor(wrapper: HTMLElement)
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +16,16 @@ export class AppComponent implements OnInit {
         this.items = [...Array(50)].map((_, i) => `Eintrag ${i}`)
     }
 
+    ngAfterViewInit() {
+        const wrapper = document.getElementsByClassName("scroller")[0] as HTMLElement
+        this.iscroll = new IScroll(wrapper)
+    }
+
     items: string[]
 
     constructor(private setup: SetupService) {}
+
+    private iscroll: any
 }
 
 // TODO: <base href="./"> for Android!

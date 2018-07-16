674b6c8e9e5111c145cf7fc09b27970efd48962e
uriegel <uriegel@hotmail.de>
2018-07-16 08:44:48 +0200

    Drawer animation with requestAnimation Frame

--- projects/umobilekit/src/lib/pipes/drawer-animation.pipe.ts
+++ projects/umobilekit/src/lib/pipes/drawer-animation.pipe.ts
@@ -6,14 +6,32 @@ import { ReplaySubject } from 'rxjs'
 })
 export class DrawerAnimationPipe implements PipeTransform {
 
+    // TODO: start value (is mot 0!!!)
+    // when back, then start value = 1!
+    // or when manual moving, start value is current value!
     transform(setPoint: number): any {
-        console.log("Pfeife", this.setPoint)
+
         if (this.setPoint != setPoint) {
             this.setPoint = setPoint
-            setTimeout(() => {
-                console.log("Gesetzt", setPoint)
-                this.actualValue.next(this.setPoint)
-            })
+            let previousTimestamp = 0
+            const animate = (timestamp: number) => {
+                if (previousTimestamp) {
+                    const timeDiff = timestamp - previousTimestamp
+                    let ratio = timeDiff / 300
+                    if (ratio > 1)
+                        ratio = 1
+                    if (ratio <= 1) {
+                        this.actualValue.next(this.setPoint * ratio)
+                        requestAnimationFrame(animate)
+                    }
+                }
+                else {
+                    previousTimestamp = timestamp
+                    requestAnimationFrame(animate)
+                }
+            }
+        
+            requestAnimationFrame(animate)
         }
         return this.actualValue
     }
